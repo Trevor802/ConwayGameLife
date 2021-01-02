27051006ca3334bc54dcbdc4c9ed361485b5bb1a
Trevor Alex <pascerveau@gmail.com>
2021-01-03 01:32:06 +0800

    A naive implementation

--- ConwayGameLife/main.cpp
+++ ConwayGameLife/main.cpp
@@ -3,6 +3,21 @@
 #include <algorithm>
 #include <set>
 
+struct TileComparator
+{
+    bool operator() (const sf::Vector2i& lhs, const sf::Vector2i& rhs) const
+    {
+        return lhs.x < rhs.x || lhs.x == rhs.x && lhs.y < rhs.y;
+    }
+};
+
+typedef std::set<sf::Vector2i, TileComparator> TileSet;
+
+static int wrapInt(int anInteger, int aModulo)
+{
+    return (anInteger + 3 * aModulo) % (2 * aModulo) - aModulo;
+}
+
 /// <summary>
 /// Get the grid lines positions
 /// </summary>
@@ -75,30 +90,139 @@ static bool pointSanityCheck(const sf::Vector2<T>& aPoint,
         && aPoint.y >= aCenter.y - aHalfRange.y && aPoint.y < aCenter.y + aHalfRange.y;
 }
 
+static const unsigned char kSideLength = 30;
+
+static int getNumOfLiveNeighbors(const sf::Vector2i& aTile, const TileSet& someTiles)
+{
+    int result = 0;
+    auto tempTile = aTile;
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y - 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y + 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+
+    tempTile.x = wrapInt(aTile.x - 1, kSideLength);
+    tempTile.y = aTile.y;
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y - 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y + 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+
+    tempTile.x = wrapInt(aTile.x + 1, kSideLength);
+    tempTile.y = aTile.y;
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y - 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    tempTile.y = wrapInt(aTile.y + 1, kSideLength);
+    if(someTiles.end() != someTiles.find(tempTile)) ++result;
+    return result;
+}
+
+static void setTileLiveness(const sf::Vector2i& aTile, TileSet& someTiles, bool isLive)
+{
+    if(isLive)
+    {
+        someTiles.insert(aTile);
+    }
+    else
+    {
+        const auto& it = someTiles.find(aTile);
+        if(someTiles.end() != it)
+            someTiles.erase(it);
+    }
+}
+
+static void getProcessingTiles(const TileSet& someTiles, TileSet& someBoundaryTiles)
+{
+    for(auto& tile : someTiles)
+    {
+        auto tempTile = tile;
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y - 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y + 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+
+        tempTile.x = wrapInt(tile.x - 1, kSideLength);
+        tempTile.y = tile.y;
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y - 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y + 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+
+        tempTile.y = tile.y;
+        tempTile.x = wrapInt(tile.x + 1, kSideLength);
+        tempTile.y = tile.y;
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y - 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+        tempTile.y = wrapInt(tile.y + 1, kSideLength);
+        someBoundaryTiles.insert(tempTile);
+    }
+}
+
+enum GameState
+{
+    Editor,
+    Automata,
+    StepByStep
+};
+
+static void processCore(TileSet& someLastLiveTiles, TileSet& someLiveTiles)
+{
+    std::swap(someLastLiveTiles, someLiveTiles);
+    someLiveTiles.clear();
+    TileSet boundaryTiles;
+    getProcessingTiles(someLastLiveTiles, boundaryTiles);
+    // Based on someLastLiveTiles, modify someLiveTiles
+    for(auto& tile : boundaryTiles)
+    {
+        int num = getNumOfLiveNeighbors(tile, someLastLiveTiles);
+        switch(num)
+        {
+        case 3:
+            setTileLiveness(tile, someLiveTiles, true);
+            break;
+        case 4:
+            setTileLiveness(tile, someLiveTiles, someLastLiveTiles.end() != someLastLiveTiles.find(tile));
+            break;
+        default:
+            setTileLiveness(tile, someLiveTiles, false);
+            break;
+        }
+    }
+}
+
 int main()
 {
     const float kSpacing = 100.f;
     const float kScrollSpeed = 0.1f;
     const float kMinScale = 1.f, kMaxScale = 10.f;
-    const unsigned char kSideLength = 10;
-    float ourWinWidth = 800.f, ourWinHeight = 600.f;
-    sf::RenderWindow window(sf::VideoMode (ourWinWidth, ourWinHeight), "Conway's Game of Life");
+    const float kAutoPeriod = 0.1f;
+    const std::string kStrTitle = "Conway's Game of Life";
+    static float ourWinWidth = 800.f, ourWinHeight = 600.f;
+    sf::RenderWindow window(sf::VideoMode (ourWinWidth, ourWinHeight), kStrTitle);
     sf::View view(sf::FloatRect(0.f, 0.f, ourWinWidth, ourWinHeight));
     window.setView(view);
-
-    bool ourMouseRightHold = false, ourMouseLeftHold = false;
-    float ourMousePressedX, ourMousePressedY;
-    float ourScale = 1.f;
-    auto cmp = [](const sf::Vector2i& a, const sf::Vector2i& b)
-    {
-        return a.x < b.x || a.x == b.x && a.y < b.y;
-    };
-    std::set<sf::Vector2i, decltype(cmp)> ourTiles(cmp);
-    bool ourHasPlaced = false;
-    sf::Vector2i ourLastChangedTile;
-
+    GameState gameState = GameState::Editor;
+    static bool ourMouseRightHold = false, ourMouseLeftHold = false;
+    static bool ourDoNextStep = false;
+    static float ourMousePressedX, ourMousePressedY;
+    static float ourScale = 1.f;
+    static TileComparator tileComparator;
+    TileSet liveTiles;
+    TileSet lastLiveTiles = liveTiles;
+    bool hasPlaced = false;
+    sf::Vector2i lastChangedTile;
+    float autoTimestamp = 0.f;
+    static float ourElapsedTime;
+    sf::Clock clock;
     while(window.isOpen())
     {
+        ourElapsedTime = clock.getElapsedTime().asSeconds();
         sf::Event event;
         while(window.pollEvent(event))
         {
@@ -162,9 +286,68 @@ int main()
             if(event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
             {
                 ourMouseLeftHold = false;
-                ourHasPlaced = false;
+                hasPlaced = false;
+            }
+            if(sf::Event::KeyPressed == event.type)
+            {
+                switch(event.key.code)
+                {
+                case sf::Keyboard::Q:
+                    if(GameState::Editor == gameState)
+                    {
+                        lastLiveTiles.clear();
+                        liveTiles.clear();
+                    }
+                    break;
+                case sf::Keyboard::Escape:
+                    window.close();
+                    break;
+                case sf::Keyboard::R:
+                    if(GameState::Automata == gameState || GameState::StepByStep == gameState)
+                        gameState = GameState::Editor;
+                    break;
+                case sf::Keyboard::Space:
+                {
+                    switch(gameState)
+                    {
+                    case Editor:
+                    case StepByStep:
+                        gameState = GameState::Automata;
+                        break;
+                    case Automata:
+                        gameState = GameState::StepByStep;
+                        break;
+                    default:
+                        break;
+                    }
+                    break;
+                }
+                case sf::Keyboard::N:
+                    gameState = GameState::StepByStep;
+                    ourDoNextStep = true;
+                    break;
+                default:
+                    break;
+                }
             }
         }
+        // Display game status
+        std::string strGameState;
+        switch(gameState)
+        {
+        case Editor:
+            strGameState = " (Editor)";
+            break;
+        case Automata:
+            strGameState = " (Automata)";
+            break;
+        case StepByStep:
+            strGameState = " (StepByStep)";
+            break;
+        default:
+            break;
+        }
+        window.setTitle(kStrTitle + strGameState);
         // Draw grid
         sf::VertexArray verticeArray;
         verticeArray.setPrimitiveType(sf::Lines);
@@ -175,35 +358,54 @@ int main()
         fillVerticeArray(sf::Vector2f(floor(left / kSpacing) * kSpacing, floor(top / kSpacing) * kSpacing),
                          sf::Vector2f(ceil(width / kSpacing) * kSpacing, ceil(height / kSpacing) * kSpacing),
                          kSpacing, verticeArray);
-        if(ourMouseLeftHold)
+        if(GameState::Editor == gameState)
         {
-            sf::Vector2f pos = view2World(win2View(sf::Vector2f(sf::Mouse::getPosition(window)),
-                                           sf::Vector2f(ourWinWidth, ourWinHeight)), view);
-            sf::Vector2i tile(floor(pos.x / kSpacing), floor(pos.y / kSpacing));
-            if(pointSanityCheck(tile, sf::Vector2i(kSideLength, kSideLength)))
+            if(ourMouseLeftHold)
             {
-                if(cmp(ourLastChangedTile, tile) || cmp(tile, ourLastChangedTile) || !ourHasPlaced)
+                sf::Vector2f pos = view2World(win2View(sf::Vector2f(sf::Mouse::getPosition(window)),
+                                              sf::Vector2f(ourWinWidth, ourWinHeight)), view);
+                sf::Vector2i tile(floor(pos.x / kSpacing), floor(pos.y / kSpacing));
+                if(pointSanityCheck(tile, sf::Vector2i(kSideLength, kSideLength)))
                 {
-                    auto it = ourTiles.find(tile);
-                    if(it != ourTiles.end())
+                    if(tileComparator(lastChangedTile, tile) || tileComparator(tile, lastChangedTile) || !hasPlaced)
                     {
-                        ourTiles.erase(it);
-                        printf("remove tile x: %d, y: %d\n", tile.x, tile.y);
+                        auto it = liveTiles.find(tile);
+                        if(it != liveTiles.end())
+                        {
+                            liveTiles.erase(it);
+                            printf("remove tile x: %d, y: %d\n", tile.x, tile.y);
+                        }
+                        else
+                        {
+                            liveTiles.insert(tile);
+                            printf("new tile x: %d, y: %d\n", tile.x, tile.y);
+                        }
+                        hasPlaced = true;
                     }
-                    else
-                    {
-                        ourTiles.insert(tile);
-                        printf("new tile x: %d, y: %d\n", tile.x, tile.y);
-                    }
-                    ourHasPlaced = true;
+                    lastChangedTile = tile;
                 }
-                ourLastChangedTile = tile;
+            }
+        }
+        else if(GameState::Automata == gameState)
+        {
+            if(ourElapsedTime - autoTimestamp > kAutoPeriod)
+            {
+                processCore(lastLiveTiles, liveTiles);
+                autoTimestamp = ourElapsedTime;
+            }
+        }
+        else if(GameState::StepByStep == gameState)
+        {
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::N) && ourDoNextStep)
+            {
+                processCore(lastLiveTiles, liveTiles);
+                ourDoNextStep = false;
             }
         }
         // Core drawing
         window.clear();
         window.draw(verticeArray);
-        for(auto& tile : ourTiles)
+        for(auto& tile : liveTiles)
         {
             sf::RectangleShape tileShape(sf::Vector2f(kSpacing, kSpacing));
             tileShape.setPosition(sf::Vector2f(tile) * kSpacing);
